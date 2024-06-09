a0ea2015de70f2ccbaaf97cd67b66e297cc4cfc7
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-06-09 11:36:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ module.exports = {
   GOODBYE_MSG: process.env.GOODBYE_MSG || "@user Left The Group",
   PACKNAME: process.env.PACKNAME || "𝙒𝞖𝞓𝙏𝙎",
   AUTHOR: process.env.AUTHOR || "𝞑𝞘𝞦𝞑𝙔",
-  SUDO: process.env.SUDO || "0,919446072492",
+  SUDO: process.env.SUDO || "919446072492",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   OWNER_NAME: process.env.OWNER_NAME || "𝙕𝞘𝙔𝞓𝞜",
