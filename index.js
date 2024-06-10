d388286e6642fe021b30a1041a96e9367319db48
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-06-10 18:23:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function GenSession() {
         body
     } = await got(`${SESSION_VALIDATOR}server/session?id=${decryptedPlainText}`)
     let result = JSON.parse(body).result[0].data;
-    fs.writeFileSync("./lib/auth_info_baileys/creds.json", result);
+    fsx.writeFileSync("./lib/auth_info_baileys/creds.json", result);
 }
 
 async function auth() {
