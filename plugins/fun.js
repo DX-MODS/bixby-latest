40f18ddcd3de03f5c3d3fa2017b17b55fad06a86
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-06-11 02:51:08 +0530

    Update fun.js

--- plugins/fun.js
+++ plugins/fun.js
@@ -1,3 +1,9 @@
+/* Copyright (C) 2024 DX-MODS.
+Licensed under the MIT License;
+you may not use this file except in compliance with the License.
+WhatsBixby - Ziyan
+*/
+
 const { Bixby, isPublic, XKCDComic, getJson } = require("../lib");
 const axios = require("axios");
 
