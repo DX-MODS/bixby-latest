02d1d43ae6bc6b20e1151cc77f790a3f3e492298
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-06-11 17:38:13 +0530

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -1,3 +1,9 @@
+/* Copyright (C) 2024 DX-MODS.
+Licensed under the MIT License;
+you may not use this file except in compliance with the License.
+WhatsBixby - Ziyan
+*/
+
 const { Bixby, isPublic } = require("../lib");
 const { isAdmin, parsedJid,fromMe } = require("../lib");
 
